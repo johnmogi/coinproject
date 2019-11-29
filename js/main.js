f3bf0c230ea6406ee3d9c3c7bc1e99692bee5fcb
johnmogi <anguru@gmail.com>
2019-11-29 23:22:40 +0200

    spinner failed, second ajax call fail

--- js/main.js
+++ js/main.js
@@ -1,72 +1,78 @@
 /// <reference path="./lib/jquery-3.4.1.js" />
+// $('body').bootstrapMaterialDesign();
+
 const tempArr = [];
+
 $(() => {
 
-    // $('body').bootstrapMaterialDesign();
+    function init() {
+        $("#stage").html("")
+        // spinner("divCoins");
+    }
+    init();
+
+
 
     $(() => {
         api.retreiveCurrencies().
         then(res => {
-            saveTheCurrencies(res.slice(0, 10))
+            saveTheCurrencies(res.slice(0, 50))
         })
-    })
 
-    function saveTheCurrencies(result) {
-        tempArr.push(result)
-        draw(result)
-    }
-
-    function draw(array) {
-
-        // $("#stage").html("")
-        for (let i = 0; i < 5; i++) {
-            drawCoin(array[i])
+        function saveTheCurrencies(result) {
+            tempArr.push(result)
+            draw(result)
         }
-    }
+
+        function getCoin(result) {
+            console.log(result)
+
+        }
+
+        function draw(array) {
+
+            for (let i = 0; i < array.length; i++) {
+                drawCoin(array[i])
+            }
+        }
 
 
-    function drawCoin(currency) {
-        // api.getCurrencyInfoById(currency).then((result) => {
+        function drawCoin(currency) {
 
-        //     const image = result.image.small;
-        //     const current_price_usd = result.market_data.current_price.usd;
-        //     const current_price_eur = result.market_data.current_price.eur;
-        //     const current_price_ils = result.market_data.current_price.ils;
-
-        // });
-        const card = `
-            <div class="card col-4" id="${currency.id}">
-            <img class="card-img-top" src="${currency.image.large}" alt="Card image cap"> 
-            <div class="card-body">
+            const card = `
+                
+            <div class="card bg-light border-dark col-4" id="${currency.id}">
+            <div class="card-body bg-transparent">
                 <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input input-cards-id" id="L+${currency.id}" value="${currency.id}">
+                    <input type="checkbox" class="custom-control-input input-cards-id" id="${currency.id}" value="${currency.id}">
                     <label class="custom-control-label" for="L+${currency.id}"></label>
                 </div>
-                <h5 class="card-title">${currency.name}</h5>
-                <p class="card-text">Symbol:${currency.symbol}</p>
+                <h5 class="card-title">${currency.symbol}</h5>
+                <p class="card-text">${currency.name}</p>
+                <img class="card-img-top" src="${currency.image.large}" alt="Card image cap"> 
+
                 <p>
-                    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button"
-                        aria-expanded="false" aria-controls="multiCollapseExample1">Toggle
-                        first element</a>
+                    <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapse+${currency.id}" role="button"
+                        aria-expanded="false" aria-controls="multiCollapse+${currency.id}">More Info</a>
 
                 </p>
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">More info</button>
-                </div>
-                <div class="collapse multi-collapse" id="multiCollapseExample1">
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    
+                <div class="collapse multi-collapse" id="multiCollapse+${currency.id}">
+                    <p class="card-text"><small class="text-muted">Last updated : <br/>${currency.last_updated}</small>
 
+                    </p>
+                    <p class="card-text">price : ${currency.market_data.current_price.usd}$</p> 
                 </div>
             </div>
             </div>
-
+       
             `
 
-        $("#stage").append(card)
+            $("#stage").append(card)
 
 
-        console.log(currency)
-    }
+            console.log(currency)
+        }
 
-
+    });
 }); //RF
--- js/sources.js
+++ js/sources.js
@@ -1,20 +1,33 @@
 /// <reference path="./lib/jquery-3.4.1.js" />
 
 const config = {
-    allCoins: "https://api.coingecko.com/api/v3/coins/list",
-    oneCoin: "https://api.coingecko.com/api/v3/coins"
+    allCoins: "https://api.coingecko.com/api/v3/coins/list/",
+    fiftyCoins: "https://api.coingecko.com/api/v3/coins/"
 }
 const api = {
     retreiveCurrencies: function () {
         return new Promise((resolve) => {
             $.ajax({
-                url: config.oneCoin,
+                url: config.fiftyCoins,
                 method: "GET",
                 success: function (currencies) {
                     resolve(currencies)
-                }
+                },
             })
         })
     }
+    // ,
+    // retrieveCurrencyId: function (id) {
+    //     return new Promise((resolve) => {
+    //         $.ajax({
+    //             url: `${config.allCoins}/${id}`,
+    //             method: "GET",
+    //             success: function (coinso) {
+    //                 resolve(coinso)
+    //             }
+    //         })
+    //     })
+
+    // }
 
 }
